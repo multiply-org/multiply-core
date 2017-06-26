4fcdd1739a8c04ebad2f5accba6d1556f6a67f5d
Alex <alexander.loew@lmu.de>
2017-06-26 13:58:39 +0200

    added state.py as skeleton

---
+++ multiply_dummy/state.py
@@ -0,0 +1,8 @@
+class State(object):
+    def __init__(self, **kwargs):
+        self.state = kwargs.get('state', None)
+        assert self.state is not None
+
+class TargetState(State):
+    def __init__(self, **kwargs):
+        super(TargetState, self).__init__(**kwargs)
