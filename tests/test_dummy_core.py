16cdec2a24f0575aca80b33b9b6fbb4c6b4ff9e6
Alex <alexander.loew@lmu.de>
2017-06-26 08:44:15 +0200

    added test routines

---
+++ tests/test_dummy_core.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+os.sep+'..')
+#from multiply_dummy import dummy_platform
+
+class TestCore(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_dummy(self):
+        assert True
+
+
+
+if __name__ == '__main__':
+    unittest.main()
