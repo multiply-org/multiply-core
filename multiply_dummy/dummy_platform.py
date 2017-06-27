09693cd97fbc3075a77008688d29c36efbcf77b9
Alex <alexander.loew@lmu.de>
2017-06-27 22:15:09 +0200

    added roughess prior

--- multiply_dummy/dummy_platform.py
+++ multiply_dummy/dummy_platform.py
@@ -29,7 +29,7 @@ ul = {'lon' : 11.2, 'lat' : 48.0}
 lr = {'lon' : 12.2, 'lat' : 45.0}
 
 tstate = TargetState(state={'lai':True, 'sm':False})
-config = Configuration(region={'ul' : ul, 'lr' : lr}, time_start=t1, time_stop=t2, tstate=tstate)   
+config = Configuration(region={'ul' : ul, 'lr' : lr}, time_start=t1, time_stop=t2, tstate=tstate, landcover='abc.nc', luts={'roughness' : 'nix.lut'})
 
 
 
