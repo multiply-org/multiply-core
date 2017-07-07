dbec6def32e9ff560ec9270f64df0b7bfea71808
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-07-07 11:26:00 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='multiply-dummy',
+      version='0.1',
+      description='MULTIPLY Dummy',
+      author='MULTIPLY Team',
+      packages=['multiply_dummy'],
+     )
