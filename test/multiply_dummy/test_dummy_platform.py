28cb31f7a9ae3970e0b96740a193478e014c4717
TonioF <tonio.fincke@brockmann-consult.de>
2017-08-18 18:29:39 +0200

    test

--- test/multiply_dummy/test_dummy_platform.py
+++ test/multiply_dummy/test_dummy_platform.py
@@ -1,15 +1,16 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+os.sep+'...')
-from multiply_dummy import dummy_platform
 import pip
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+os.sep+'...')
+print(sys.path)
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+                                  for i in installed_packages])
+print(installed_packages_list)
+from multiply_dummy import dummy_platform
 
 def test_dummy():
-    print(sys.path)
-    installed_packages = pip.get_installed_distributions()
-    installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-                                      for i in installed_packages])
-    print(installed_packages_list)
+
     # """
     # we simply run the platform dummy
     # """
