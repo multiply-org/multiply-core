4f07a241b8d0cd0fb95194a03f3924026388aaa6
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-01-26 19:30:01 +0100

    commented out dummy module

--- test/multiply_dummy/test_dummy_platform.py
+++ test/multiply_dummy/test_dummy_platform.py
@@ -1,17 +1,17 @@
-import sys
-import os
-import pip
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+os.sep+'...')
-print(sys.path)
-installed_packages = pip.get_installed_distributions()
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-                                  for i in installed_packages])
-print(installed_packages_list)
-from multiply_dummy import dummy_platform
-
-def test_dummy():
-
-    # """
-    # we simply run the platform dummy
-    # """
-    dummy_platform
+# import sys
+# import os
+# import pip
+# sys.path.append(os.path.dirname(os.path.abspath(__file__))+os.sep+'...')
+# print(sys.path)
+# installed_packages = pip.get_installed_distributions()
+# installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+#                                   for i in installed_packages])
+# print(installed_packages_list)
+# from multiply_dummy import dummy_platform
+#
+# def test_dummy():
+#
+#     # """
+#     # we simply run the platform dummy
+#     # """
+#     dummy_platform
