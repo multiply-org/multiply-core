f6138b428fc2caedf45327147ba9dbc1debdc5b9
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-02-15 16:28:53 +0100

    adapted requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 
 from setuptools import setup
 
+requirements = [
+    'gdal',
+    'osr'
+]
+
 setup(name='multiply-core',
       version='0.1',
       description='MULTIPLY Core',
       author='MULTIPLY Team',
-      packages=['multiply_core', 'multiply_core.util']
-     )
+      packages=['multiply_core', 'multiply_core.util'],
+      install_requires=requirements
+)
