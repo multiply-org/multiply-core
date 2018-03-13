9bb052cdf765d886c854e024508123f583896221
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-03-13 17:36:08 +0100

    added reprojection functionality

--- multiply_core/util/reproject.py
+++ multiply_core/util/reproject.py
@@ -1,57 +1,154 @@
 import gdal
+import numpy as np
 import osr
+from typing import Optional, Sequence, Union
 
 __author__ = "José Luis Gómez-Dans (University College London)," \
              "Tonio Fincke (Brockmann Consult GmbH)"
 
 
-class Reproject(object):
+def transform_coordinates(source: osr.SpatialReference, target: osr.SpatialReference,
+                          coords: Sequence[float]) -> Sequence[float]:
+    """
+    Returns coordinates in a target reference system that have been transformed from coordinates
+    in the source reference system.
+    :param source: The source spatial reference system
+    :param target: The target spatial reference system
+    :param coords: The coordinates to be transformed. Coordinates are expected in a sequence
+    [x1, y1, x2, y2, ..., xn, yn].
+    :return: The transformed coordinates. Will be in a sequence [x1, y1, x2, y2, ..., xn, yn],
+    as the source coordinates.
+    """
+    num_coords = int(len(coords) / 2)
+    target_coords = []
+    coordinate_transformation = osr.CoordinateTransformation(source, target)
+    for i in range(num_coords):
+        target_coord = coordinate_transformation.TransformPoint(coords[i * 2], coords[i * 2 + 1])
+        target_coords.append(target_coord[0])
+        target_coords.append(target_coord[1])
+    return target_coords
 
-    @staticmethod
-    def reproject_image(source_img, target_img, dstSRSs=None):
-        """Reprojects/Warps an image to fit exactly another image.
-        Additionally, you can set the destination SRS if you want
-        to or if it isn't defined in the source image."""
-        if type(target_img) is str:
-            g = gdal.Open(target_img)
-        else:
-            g = target_img
-        if type(source_img) is str:
-            s = gdal.Open(source_img)
-        else:
-            s = source_img
-        geo_t = g.GetGeoTransform()
-        x_size, y_size = g.RasterXSize, g.RasterYSize
-        xmin = min(geo_t[0], geo_t[0] + x_size * geo_t[1])
-        xmax = max(geo_t[0], geo_t[0] + x_size * geo_t[1])
-        ymin = min(geo_t[3], geo_t[3] + y_size * geo_t[5])
-        ymax = max(geo_t[3], geo_t[3] + y_size * geo_t[5])
-        xRes, yRes = abs(geo_t[1]), abs(geo_t[5])
-        if dstSRSs is None:
-            dstSRS = osr.SpatialReference()
-            raster_wkt = g.GetProjection()
-            dstSRS.ImportFromWkt(raster_wkt)
-        else:
-            dstSRS = dstSRSs
-        g = gdal.Warp('', s, format='MEM', outputBounds=[xmin, ymin, xmax, ymax], xRes=xRes, yRes=yRes, dstSRS=dstSRS)
-        return g
 
-    # @staticmethod
-    # def resample_image_to_resolution(self, image, xRes: float, yRes: float):
-    #     """
-    #     Reprojects/Warps an image to the given projection.
-    #     Additionally, you can set the destination SRS if you want
-    #     to or if it isn't defined in the source image.
-    #     :return:
-    #     """
-    #     dataset = gdal.Open(image)
-    #     proj = dataset.GetProjection()
-    #     geoT = np.array(dataset.GetGeoTransform())
-    #     new_geoT = geoT * 1.
-    #     new_geoT[1] = xRes
-    #     new_geoT[5] = -yRes
-    #     dataset = gdal.Warp('', image, format='MEM',
-    #                   outputBounds=[xmin, ymin, xmax, ymax], xRes=xRes, yRes=yRes,
-    #                   dstSRS=dstSRS)
-    #
-    #     return proj, new_geoT.tolist()
+def get_spatial_reference_system_from_dataset(dataset: gdal.Dataset) -> osr.SpatialReference:
+    """
+    Returns the spatial reference system of a dataset
+    :param dataset: A dataset
+    :return: The spatial reference system of the dataset
+    """
+    projection = dataset.GetProjection()
+    srs = osr.SpatialReference()
+    srs.ImportFromWkt(projection)
+    return srs
+
+
+def get_target_resolutions(dataset: gdal.Dataset) -> (float, float):
+    """
+    Returns the resolution parameters of the geographic transform used by this dataset.
+    :param dataset: A dataset
+    :return: The resolution parameters of the dataset. The first value refers to the x-axis,
+    the second one to the y-axis.
+    """
+    geo_transform = dataset.GetGeoTransform()
+    return geo_transform[1], -geo_transform[5]
+
+
+def reproject_dataset(dataset: Union[str, gdal.Dataset], bounds: Sequence[float], x_res: int, y_res: int,
+                      destination_srs: osr.SpatialReference, bounds_srs: Optional[osr.SpatialReference],
+                      resampling_mode: Optional[str]) -> gdal.Dataset:
+    """
+    Reprojects a gdal dataset to a reference system with the given bounds and the given spatial resolution.
+    :param dataset: A dataset
+    :param bounds: A 1-d float array specifying the bounds of the resulting dataset. Must consist of the following
+    four float values: xmin, ymin, xmax, ymax.
+    :param x_res: The resolution the resulting dataset shall have in x-direction. Must be set in accordance to
+    destination_srs.
+    :param y_res: The resolution the resulting dataset shall have in y-direction. Must be set in accordance to
+    destination_srs.
+    :param destination_srs: The spatial reference system that the resulting data set shall show.
+    :param bounds_srs: The spatial reference system in which the bounds are specified. If not given, it is assumed
+    that the bounds are given in the destination_srs.
+    :param resampling_mode: The mode by which the values from the source dataset shall be combined to values in the
+    target dataset. Available modes are:
+    * near (Nearest Neighbour)
+    * bilinear
+    * cubic
+    * cubicspline
+    * lanczos
+    * average
+    * mode
+    * max
+    * min
+    * med
+    * q1
+    * q3
+    If none is selected, 'bilinear' will be selected in case the source values need to be sampled up to a finer
+    destination resolution and 'average' in case the values need to be sampled down to a coarser destination resolution.
+    :return: A spatial dataset with the chosen destination spatial reference system, in the bounds and the x- and y-
+    resolutions that have been set.
+    """
+    if type(dataset) is str:
+        dataset = gdal.Open(dataset)
+    if bounds_srs is None:
+        bounds_srs = destination_srs
+    if resampling_mode is None:
+        resampling_mode = _get_resampling(dataset, bounds, x_res, y_res, bounds_srs, destination_srs)
+    warp_options = gdal.WarpOptions(format='Mem', outputBounds=bounds, outputBoundsSRS=bounds_srs,
+                                    xRes=x_res, yRes=y_res, dstSRS=destination_srs, resampleAlg=resampling_mode)
+    reprojected_data_set = gdal.Warp('', dataset, options=warp_options)
+    return reprojected_data_set
+
+
+def _get_resampling(dataset: gdal.Dataset, bounds: Sequence[float], x_res: float, y_res: float,
+                    bounds_srs: osr.SpatialReference, destination_srs: osr.SpatialReference) -> str:
+    if _need_to_sample_up(dataset, bounds, x_res, y_res, bounds_srs, destination_srs):
+        return 'bilinear'
+    else:
+        return 'average'
+
+
+def _need_to_sample_up(dataset: gdal.Dataset, bounds: Sequence[float], x_res: float, y_res: float,
+                       bounds_srs: osr.SpatialReference, destination_srs: osr.SpatialReference) -> bool:
+    source_srs = get_spatial_reference_system_from_dataset(dataset)
+    bounds_in_source_coordinates = transform_coordinates(bounds_srs, source_srs, bounds)
+    source_resolutions = get_target_resolutions(dataset)
+    source_resolution_measure = _get_dist_measure(bounds_in_source_coordinates,
+                                                  source_resolutions[0], source_resolutions[1])
+    bounds_in_dest_coordinates = transform_coordinates(bounds_srs, destination_srs, bounds)
+    dest_resolution_measure = _get_dist_measure(bounds_in_dest_coordinates, x_res, y_res)
+    return dest_resolution_measure > source_resolution_measure
+
+
+def _get_dist_measure(source_coordinates: Sequence[float], x_res: float, y_res: float):
+    x_dist = np.sqrt(np.square(source_coordinates[0] - source_coordinates[2]))
+    y_dist = np.sqrt(np.square(source_coordinates[1] - source_coordinates[3]))
+    return (x_dist / x_res) * (y_dist / y_res)
+
+
+def reproject_image(source_img, target_img, dstSRSs=None):
+    #TODO: replace this method with the other functionality in this module
+    """Reprojects/Warps an image to fit exactly another image.
+    Additionally, you can set the destination SRS if you want
+    to or if it isn't defined in the source image."""
+    if type(target_img) is str:
+        g = gdal.Open(target_img)
+    else:
+        g = target_img
+    if type(source_img) is str:
+        s = gdal.Open(source_img)
+    else:
+        s = source_img
+    geo_t = g.GetGeoTransform()
+    x_size, y_size = g.RasterXSize, g.RasterYSize
+    xmin = min(geo_t[0], geo_t[0] + x_size * geo_t[1])
+    xmax = max(geo_t[0], geo_t[0] + x_size * geo_t[1])
+    ymin = min(geo_t[3], geo_t[3] + y_size * geo_t[5])
+    ymax = max(geo_t[3], geo_t[3] + y_size * geo_t[5])
+    xRes, yRes = abs(geo_t[1]), abs(geo_t[5])
+    if dstSRSs is None:
+        dstSRS = osr.SpatialReference()
+        raster_wkt = g.GetProjection()
+        dstSRS.ImportFromWkt(raster_wkt)
+    else:
+        dstSRS = dstSRSs
+    g = gdal.Warp('', s, format='MEM', outputBounds=[xmin, ymin, xmax, ymax], xRes=xRes, yRes=yRes, dstSRS=dstSRS)
+    return g
--- test/core/test_reproject.py
+++ test/core/test_reproject.py
@@ -1,55 +1,126 @@
 import gdal
-import numpy as np
-import os
 import osr
+import multiply_core.util.reproject as reproject
+import pytest
 
 __author__ = "Tonio Fincke (Brockmann Consult GmbH)"
 
 S2_FILE = './test/test_data/T32UME_20170910T104021_B10.jp2'
-S2_TIFF_FILE = './test/test_data/T32UME_20170910T104021_B10.tiff'
-LAI_TIFF_FILE = './test/test_data/Priors_lai_125_[50_60N]_[000_010E].tiff'
 ALA_TIFF_FILE = './test/test_data/Priors_ala_125_[50_60N]_[000_010E].tiff'
-GLOBAL_VRT_FILE = './test/test_data/Priors_lai_060_global.vrt'
+
+EPSG_4326_WKT = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433],AUTHORITY["EPSG","4326"]]'
+EPSG_32632_WKT = 'PROJCS["WGS 84 / UTM zone 32N",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Latitude",NORTH],AXIS["Longitude",EAST],AUTHORITY["EPSG","4326"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",9],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","32632"]]'
+EPSG_32232_WKT = 'PROJCS["WGS 72 / UTM zone 32N",GEOGCS["WGS 72",DATUM["World Geodetic System 1972",SPHEROID["WGS 72",6378135.0,298.26,AUTHORITY["EPSG","7043"]],TOWGS84[0.0,0.0,4.5,0.0,0.0,0.554,0.219],AUTHORITY["EPSG","6322"]],PRIMEM["Greenwich",0.0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.017453292519943295],AXIS["Geodetic longitude",EAST],AXIS["Geodetic latitude",NORTH],AUTHORITY["EPSG","4322"]],PROJECTION["Transverse_Mercator",AUTHORITY["EPSG","9807"]],PARAMETER["central_meridian",9.0],PARAMETER["latitude_of_origin",0.0],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000.0],PARAMETER["false_northing",0.0],UNIT["m",1.0],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","32232"]]'
 
 
-def test_resample_image_to_resolution():
-    xRes = 60.
-    yRes = 10.
-    vrt_dataset = gdal.Open(GLOBAL_VRT_FILE)
+def test_transform_coordinates_0():
+    ala_dataset = gdal.Open(ALA_TIFF_FILE)
+    ala_srs = reproject.get_spatial_reference_system_from_dataset(ala_dataset)
     s2_dataset = gdal.Open(S2_FILE)
-    s2_projection = s2_dataset.GetProjection()
-    s2_transform = s2_dataset.GetGeoTransform()
+    s2_srs = reproject.get_spatial_reference_system_from_dataset(s2_dataset)
 
-    x_size, y_size = s2_dataset.RasterXSize, s2_dataset.RasterYSize
-    xmin = min(s2_transform[0], s2_transform[0] + x_size * s2_transform[1])
-    xmax = max(s2_transform[0], s2_transform[0] + x_size * s2_transform[1])
-    ymin = min(s2_transform[3], s2_transform[3] + y_size * s2_transform[5])
-    ymax = max(s2_transform[3], s2_transform[3] + y_size * s2_transform[5])
+    coords = [-0.0013889, 60.0013885,
+              -0.0013889, 50.0038300,
+              9.9986114, 60.0013885,
+              9.9986114, 50.0038300]
+    transformed_coordinates = reproject.transform_coordinates(ala_srs, s2_srs, coords)
+    assert 8 == len(transformed_coordinates)
+    assert -144583.384, transformed_coordinates[0]
+    assert 6685755.131, transformed_coordinates[1]
+    assert -144583.384, transformed_coordinates[2]
+    assert 5539163.063, transformed_coordinates[3]
+    assert 571659.159, transformed_coordinates[4]
+    assert 6685755.131, transformed_coordinates[5]
+    assert 571659.159, transformed_coordinates[6]
+    assert 5539163.063, transformed_coordinates[7]
 
-    srs = osr.SpatialReference()
-    srs.ImportFromWkt(s2_projection)
-    reprojected_tiff = gdal.Warp('', vrt_dataset,
-                                 format='Mem',
-                                 outputBounds=[xmin, ymin, xmax, ymax],
-                                 xRes=xRes, yRes=yRes, dstSRS=srs)
-    reprojected_projection = reprojected_tiff.GetProjection()
-    reprojected_transform = reprojected_tiff.GetGeoTransform()
-    raster_width = reprojected_tiff.RasterXSize
-    raster_height = reprojected_tiff.RasterYSize
-    reprojected_tiff_data = reprojected_tiff.GetRasterBand(1).ReadAsArray()
 
-    driver = gdal.GetDriverByName('GTiff')
-    s2_file = driver.Create(S2_TIFF_FILE, raster_width, raster_height, 1, gdal.GDT_Float32,
-                            ['COMPRESS=DEFLATE', 'BIGTIFF=YES', 'PREDICTOR=1', 'TILED=YES'])
-    s2_file.SetProjection(reprojected_projection)
-    s2_file.SetGeoTransform(reprojected_transform)
+def test_transform_coordinates_1():
+    ala_dataset = gdal.Open(ALA_TIFF_FILE)
+    ala_srs = reproject.get_spatial_reference_system_from_dataset(ala_dataset)
+    s2_dataset = gdal.Open(S2_FILE)
+    s2_srs = reproject.get_spatial_reference_system_from_dataset(s2_dataset)
 
-# def
+    coords = [-0.0013889, 60.0013885, 9.9986114, 50.0038300]
+    transformed_coordinates = reproject.transform_coordinates(ala_srs, s2_srs, coords)
+    assert 4 == len(transformed_coordinates)
+    assert -144583.384, transformed_coordinates[0]
+    assert 6685755.131, transformed_coordinates[1]
+    assert 571659.159, transformed_coordinates[2]
+    assert 5539163.063, transformed_coordinates[3]
 
-    tile_width = 256
-    tile_height = 256
-    for y in range(0, raster_height, tile_height):
-        y_end = min(y + tile_height, raster_height)
-        for x in range(0, raster_width, tile_width):
-            x_end = min(x + tile_height, raster_width)
-            s2_file.GetRasterBand(1).WriteArray(reprojected_tiff_data[y:y_end, x:x_end], xoff=x, yoff=y)
+
+def test_get_spatial_reference_system_from_dataset():
+    ala_dataset = gdal.Open(ALA_TIFF_FILE)
+    ala_srs = reproject.get_spatial_reference_system_from_dataset(ala_dataset)
+    assert EPSG_4326_WKT == ala_srs.ExportToWkt()
+
+    s2_dataset = gdal.Open(S2_FILE)
+    s2_srs = reproject.get_spatial_reference_system_from_dataset(s2_dataset)
+    assert EPSG_32632_WKT == s2_srs.ExportToWkt()
+
+
+def test_get_target_resolutions():
+    ala_dataset = gdal.Open(ALA_TIFF_FILE)
+    x_resolution, y_resolution = reproject.get_target_resolutions(ala_dataset)
+    assert pytest.approx(0.00277778) == x_resolution
+    assert pytest.approx(0.0027771) == y_resolution
+
+    s2_dataset = gdal.Open(S2_FILE)
+    x_resolution, y_resolution = reproject.get_target_resolutions(s2_dataset)
+    assert 60 == x_resolution
+    assert 60 == y_resolution
+
+
+def test_get_resampling():
+    ala_dataset = gdal.Open(ALA_TIFF_FILE)
+    bounds = [-9.220204779005144, 48.98786207315579, -8.220204897166695, 49.28786209826107]
+    bounds_srs = osr.SpatialReference()
+    bounds_srs.ImportFromWkt(EPSG_32232_WKT)
+    dest_srs = osr.SpatialReference()
+    dest_srs.ImportFromWkt(EPSG_32632_WKT)
+    assert 'bilinear' == reproject._get_resampling(ala_dataset, bounds, 100.0, 100.0, bounds_srs, dest_srs)
+    assert 'average' == reproject._get_resampling(ala_dataset, bounds, 1000.0, 1000.0, bounds_srs, dest_srs)
+
+
+def test_need_to_sample_up():
+    ala_dataset = gdal.Open(ALA_TIFF_FILE)
+    bounds = [-9.220204779005144, 48.98786207315579, -8.220204897166695, 49.28786209826107]
+    bounds_srs = osr.SpatialReference()
+    bounds_srs.ImportFromWkt(EPSG_32232_WKT)
+    dest_srs = osr.SpatialReference()
+    dest_srs.ImportFromWkt(EPSG_32632_WKT)
+    assert reproject._need_to_sample_up(ala_dataset, bounds, 100.0, 100.0, bounds_srs, dest_srs)
+    assert not reproject._need_to_sample_up(ala_dataset, bounds, 1000.0, 1000.0, bounds_srs, dest_srs)
+
+
+def test_get_dist_measure():
+    assert 8.0 == reproject._get_dist_measure([50.0, 10.0, 100.0, 50.0], 25.0, 10.0)
+
+
+def test_reproject_image():
+    destination_srs = osr.SpatialReference()
+    destination_srs.ImportFromWkt(EPSG_32232_WKT)
+    bounds_srs = osr.SpatialReference()
+    bounds_srs.SetWellKnownGeogCS('EPSG:4326')
+    bounds = [7.8, 53.5, 8.8, 53.8]
+    reprojected_dataset = reproject.reproject_dataset(S2_FILE, bounds, x_res=50, y_res=100,
+                                                      destination_srs=destination_srs,
+                                                      bounds_srs=bounds_srs,
+                                                      resampling_mode=None)
+    assert 1328 == reprojected_dataset.RasterXSize
+    assert 327 == reprojected_dataset.RasterYSize
+    assert EPSG_32232_WKT == reprojected_dataset.GetProjection()
+    geo_transform = reprojected_dataset.GetGeoTransform()
+    assert 6 == len(geo_transform)
+    assert 420392.4558445791 == geo_transform[0]
+    assert 50.0 == geo_transform[1]
+    assert 0.0 == geo_transform[2]
+    assert 5961284.037740353 == geo_transform[3]
+    assert 0.0 == geo_transform[4]
+    assert -100.0 == geo_transform[5]
+    assert 1 == reprojected_dataset.RasterCount
+    raster_band = reprojected_dataset.GetRasterBand(1)
+    raster_data = raster_band.ReadAsArray()
+    assert 8 == raster_data[0][0]
+    assert 14 == raster_data[94][285]
