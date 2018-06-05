7f8f15fdf662995b328fb7cffdc340f2b7c51d4d
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-06-05 11:38:14 +0200

    comply to pep8

--- multiply_core/observations/__init__.py
+++ multiply_core/observations/__init__.py
@@ -1,4 +1,3 @@
-from .observations import ProductObservations, ObservationData, ProductObservationsCreator, add_observations_creator_to_registry, \
-    create_observations, sort_file_ref_list
-from .s2_observations import S2Observations, S2ObservationsCreator, \
-    extract_angles_from_metadata_file, extract_tile_id
+from .observations import ProductObservations, ObservationData, ProductObservationsCreator, \
+    add_observations_creator_to_registry, create_observations, sort_file_ref_list
+from .s2_observations import S2Observations, S2ObservationsCreator, extract_angles_from_metadata_file, extract_tile_id
