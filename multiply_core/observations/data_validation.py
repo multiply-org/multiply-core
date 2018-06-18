2f66beacd9a862da0db45696e9cbe6b144f99ed2
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-06-18 20:30:33 +0200

    added data validation

--- multiply_core/observations/data_validation.py
+++ multiply_core/observations/data_validation.py
@@ -13,7 +13,7 @@ __author__ = 'Tonio Fincke (Brockmann Consult GmbH)'
 from abc import ABCMeta, abstractmethod
 
 
-class DataChecker(metaclass=ABCMeta):
+class DataValidator(metaclass=ABCMeta):
 
     @classmethod
     def name(cls) -> str:
@@ -23,11 +23,29 @@ class DataChecker(metaclass=ABCMeta):
     def is_valid(self, path: str) -> bool:
         """Whether the data at the given path is a valid data product for the type."""
 
-class AWS_S2_Checker(DataChecker):
+class AWS_S2_Validator(DataValidator):
+    # 29\S\QB\2017\9\4\0
 
     @classmethod
     def name(cls) -> str:
         return 'AWS_S2_L1C'
 
     def is_valid(self, path: str) -> bool:
-        return True
+        return False
+
+
+class Validation(object):
+
+    def __init__(self):
+        self.validators = []
+        # TODO replace this with framework
+        self.validators.append(AWS_S2_Validator())
+
+    def add_validator(self, validator: DataValidator):
+        self.validators.append(validator)
+
+    def get_valid_type(self, path: str) -> str:
+        for validator in self.validators:
+            if validator.is_valid(path):
+                return validator.name()
+        return ''
