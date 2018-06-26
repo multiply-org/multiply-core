774063d95aa5eedc5e45eebfd7f4affea04a1dac
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-06-26 14:27:04 +0200

    import observationswrapper

--- multiply_core/observations/__init__.py
+++ multiply_core/observations/__init__.py
@@ -1,3 +1,4 @@
-from .observations import ProductObservations, ObservationData, ProductObservationsCreator, ObservationsFactory
+from .observations import ProductObservations, ObservationData, ProductObservationsCreator, ObservationsFactory, \
+    ObservationsWrapper
 from .s2_observations import S2Observations, S2ObservationsCreator, extract_angles_from_metadata_file, extract_tile_id
 from .data_validation import DataValidator, add_validator, get_valid_type, get_valid_types
