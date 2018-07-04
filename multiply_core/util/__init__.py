c979befb168bbe7fcb4795acf62834992ead1869
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-07-04 15:39:22 +0200

    updated init py

--- multiply_core/util/__init__.py
+++ multiply_core/util/__init__.py
@@ -1,4 +1,5 @@
-from .util import AttributeDict, FileRef, compute_distance, get_time_from_string, get_days_of_month, is_leap_year, get_mime_type
+from .util import AttributeDict, FileRef, compute_distance, get_time_from_string, get_days_of_month, is_leap_year, \
+    get_mime_type, block_diag
 from .reproject import transform_coordinates, get_spatial_reference_system_from_dataset, get_target_resolutions, \
     reproject_dataset, reproject_image, Reprojection
 from .file_ref_creation import FileRefCreation
