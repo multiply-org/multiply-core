92f19325009bf4c8626585084b76c9307503be59
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-07-05 12:19:54 +0200

    added test

--- test/core/test_util.py
+++ test/core/test_util.py
@@ -61,3 +61,21 @@ def test_block_diag():
         for i in range(value_length):
             for j in range(value_length):
                 assert matrices[k][i][j] == coo_matrix_array[k * value_length + i][k * value_length + j]
+
+def test_block_as_numpy_matrix():
+    matrices = np.empty((3, 10, 10), dtype=np.float32)
+    values_1 = np.array([0.01, 0.2, 0.01, 0.05, 0.01, 0.01, 0.50, 0.1, 0.1, 0.1])
+    values_2 = np.array([0.02, 0.18, 0.04, 0.07, 0.02, 0.03, 0.30, 0.12, 0.12, 0.15])
+    values_3 = np.array([0.005, 0.23, 0.02, 0.03, 0.04, 0.05, 0.40, 0.09, 0.13, 0.2])
+    matrices[0] = np.diag(values_1 ** 2).astype(np.float32)
+    matrices[1] = np.diag(values_2 ** 2).astype(np.float32)
+    matrices[2] = np.diag(values_3 ** 2).astype(np.float32)
+    coo_matrix = util.block_diag(matrices)
+    coo_matrix_array = coo_matrix.toarray()
+    value_length = 10
+    total_length = value_length * 3
+    assert (total_length, total_length) == coo_matrix.shape
+    for k in range(3):
+        for i in range(value_length):
+            for j in range(value_length):
+                assert matrices[k][i][j] == coo_matrix_array[k * value_length + i][k * value_length + j]
