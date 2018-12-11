d72eddb31c514397c371dbb3239d2a6b37f369f3
TonioF <tonio.fincke@brockmann-consult.de>
2018-12-11 11:15:14 +0100

    continued work on geotiff writer

--- multiply_core/observations/output.py
+++ multiply_core/observations/output.py
@@ -15,7 +15,7 @@ class Writer(metaclass=ABCMeta):
 
     @abstractmethod
     def init(self, file_names: List[str], geo_transform: tuple, projection: str, width: int, height: int,
-             num_bands: Optional[List[int]], data_types: Optional[List[str]]):
+             num_bands: Optional[List[int]] = None, data_types: Optional[List[str]] = None):
         pass
 
     @abstractmethod
@@ -31,14 +31,14 @@ class Writer(metaclass=ABCMeta):
 class GeoTiffWriter(Writer):
 
     def init(self, file_names: List[str], geo_transform: tuple, projection: str, width: int, height: int,
-             num_bands: Optional[List[int]], data_types: Optional[List[str]]):
+             num_bands: Optional[List[int]] = None, data_types: Optional[List[str]] = None):
         if num_bands is None or len(num_bands) == 0:
-            num_bands = np.full(len(file_names), 1)
+            num_bands = [1] * len(file_names)
         elif len(num_bands) != len(file_names):
             raise ValueError('List with number of bands must be of same size as list of file names')
         self.num_bands = num_bands
         if data_types is None or len(data_types) == 0:
-            data_types = np.full(len(file_names), 1)
+            data_types = ['Float'] * len(file_names)
         elif len(data_types) != len(file_names):
             raise ValueError('List with data types must be of same size as list of file names')
         gdal_data_types = []
@@ -54,7 +54,7 @@ class GeoTiffWriter(Writer):
                 os.makedirs(parent_dir)
             if not file_name.endswith('.tif') and not file_name.endswith('tiff'):
                 file_name = file_name + '.tif'
-            data_set = drv.Create(file_name, height, width, num_bands[i], gdal_data_types[i],
+            data_set = drv.Create(file_name, width, height, num_bands[i], gdal_data_types[i],
                                   ['COMPRESS=DEFLATE', 'BIGTIFF=YES', 'PREDICTOR=1', 'TILED=YES'])
             data_set.SetProjection(projection)
             data_set.SetGeoTransform(geo_transform)
@@ -79,12 +79,12 @@ class GeoTiffWriter(Writer):
             height = self._height
         for i, d in enumerate(data):
             if d.shape == (width * height, ):
-                d = d.reshape(width, height)
+                d = d.reshape(height, width)
             elif d.shape == (self.num_bands[i], width * height):
-                d = d.reshape(self.num_bands[i], width, height)
-            if d.shape == (self.num_bands[i], width, height) and self.num_bands[i] == 1:
-                d = d.reshape(width, height)
-            assert d.shape == (width, height) or d.shape == (self.num_bands[i], width, height)
+                d = d.reshape(self.num_bands[i], height, width)
+            if d.shape == (self.num_bands[i], height, width) and self.num_bands[i] == 1:
+                d = d.reshape(height, width)
+            assert d.shape == (height, width) or d.shape == (self.num_bands[i], height, width)
             if self.num_bands[i] > 1:
                 for band in range(self.num_bands[i]):
                     self._destination_data_sets[i].GetRasterBand(band + 1).WriteArray(d[band])
