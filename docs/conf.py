5896c6629d6997ca883c8bcf79c94e113dcb9f71
McWhity <weiss.thomas@lmu.de>
2019-01-30 18:16:34 +0100

    debug docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ MOCK_MODULES = ['bs4', 'html5lib', 'multiply_core.util', 'multiply_core.observat
                 'osr', 'sentinelhub']
 
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
