25436b8a3c2c5b7fd6664540ca3cfbf145885da4
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-03-11 17:29:30 +0100

    do not export get default variables

--- multiply_core/variables/__init__.py
+++ multiply_core/variables/__init__.py
@@ -1,1 +1,1 @@
-from .variables import get_default_variables, get_registered_variables, get_registered_variable, Variable
+from .variables import get_registered_variables, get_registered_variable, Variable
