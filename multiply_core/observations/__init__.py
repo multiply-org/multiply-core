a786886e7d06645aa6b4f0685a69bea5fcd4c146
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-12-20 12:56:22 +0100

    edited init

--- multiply_core/observations/__init__.py
+++ multiply_core/observations/__init__.py
@@ -4,5 +4,5 @@ from .s2_observations import S2Observations, S2ObservationsCreator, extract_angl
 from .data_validation import INPUT_TYPES, DataTypeConstants, DataValidator, add_validator, get_valid_type, \
     get_valid_types, get_data_type_path, is_valid, is_valid_for, get_file_pattern, get_relative_path, differs_by_name, \
     get_types_of_unprocessed_data_for_model_data_type, get_types_of_preprocessed_data_for_model_data_type, \
-    SENTINEL_1_MODEL_DATA_TYPE, SENTINEL_2_MODEL_DATA_TYPE
+    SENTINEL_1_MODEL_DATA_TYPE, SENTINEL_2_MODEL_DATA_TYPE, get_valid_files
 from .output import GeoTiffWriter
