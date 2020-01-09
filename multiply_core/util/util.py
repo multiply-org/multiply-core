09bae4eb14ab2c1a37b04f502263d6890242232a
TonioF <tonio.fincke@brockmann-consult.de>
2020-01-09 12:35:18 +0100

    added time format

--- multiply_core/util/util.py
+++ multiply_core/util/util.py
@@ -122,7 +122,8 @@ def get_time_from_string(time_string: str, adjust_to_last_day: bool = False) -> 
     """
     if time_string == '':
         return None
-    format_to_timedelta = [("%Y%m%dT%H%M%S", timedelta(), False),
+    format_to_timedelta = [("%Y%j", timedelta(hours=24, seconds=-1), False),
+                           ("%Y%m%dT%H%M%S", timedelta(), False),
                            ("%Y-%m-%dT%H:%M:%S.%fZ", timedelta(), False),
                            ("%Y-%m-%dT%H:%M:%S.%f", timedelta(), False),
                            ("%Y-%m-%dT%H:%M:%SZ", timedelta(), False),
